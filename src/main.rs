a9cf7cfab902a464dcc09be844bdf0a4277ee909
Tristan Otterpohl <Tristan.Oterpohl@Outlook.com>
2021-10-16 21:34:43 +0100

    Added extra test case

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,12 @@ mod mt940;
 mod tags;
 
 fn main() {
-    let filename = "./tests/basic_test_2.txt";
+    let filename = "./tests/basic_test_1.txt";
 
     let content: &str = &fs::read_to_string(filename).expect("Unable to read file");
     let mut swift = mt940::Mt940::new(content);
 
     swift.parse();
 
-    println!("swift = {:#?}", swift.block_text);
+    println!("swift = {:#?}", swift);
 }
